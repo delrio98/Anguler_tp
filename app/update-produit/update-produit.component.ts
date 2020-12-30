ed86ed473891a0c7dd05032198477c5830dec468
delrio98 <waelrassa27@gmail.com>
2020-12-30 17:23:05 +0100

    'master'

--- app/update-produit/update-produit.component.ts
+++ app/update-produit/update-produit.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute } from '@angular/router';
+import {ActivatedRoute, Router } from '@angular/router';
 import {ProduitService} from '../services/produit.service';
 import {Produit} from '../model/produit.model';
 @Component({
